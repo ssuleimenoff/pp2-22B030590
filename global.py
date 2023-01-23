9821509d8bb7602642a98da087676757d7f2a10b
Ayan <ay_suleimenov@kbtu.kz>
2023-01-23 23:26:01 +0600

    hi

---
+++ global.py
@@ -0,0 +1,18 @@
+# x = "awesome"
+#
+# def myfunc():
+#     x = "fantastic"
+#     print("Python is" + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
