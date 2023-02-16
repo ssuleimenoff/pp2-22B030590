f72f960097cddaf8f535223f6cd068889726ce0a
ssuleimenoff <123289263+ssuleimenoff@users.noreply.github.com>
2023-02-16 13:27:46 +0600

    Add files via upload

---
+++ tsis_4/date/task3.py
@@ -0,0 +1,6 @@
+import datetime
+
+day = datetime.datetime.today().replace(microsecond = 0)
+print()
+print(day)
+print()
---
+++ tsis_4/generators/gen1.py
@@ -0,0 +1,3 @@
+squares_generator = (i * i for i in range(int(input())))
+for i in squares_generator:
+    print(i)
